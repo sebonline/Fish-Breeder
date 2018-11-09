def93b800586a533ebb42421e4be270407836672
sebonline <42711836+sebonline@users.noreply.github.com>
2018-11-09 23:07:37 +0100

    Initial commit

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Fish", menuName = "Fish")]
+public class Fish : ScriptableObject {
+
+    public new string name;
+    public string description;
+    public float price;
+    public int maxAge;
+    public float currentAge;
+    public Sprite artwork;
+    public float minPH;
+    public float maxPH;
+    public float minTemp;
+    public float maxTemp;
+    public float maxSize;
+    
+    
+
+    enum FishType
+    {
+        Goldfish,
+        Cichlid,
+        Catfish,
+        Tetra,
+        Barb,
+        Loach,
+        Clownfish,
+        Damsel,
+        Chromis,
+        Tang,
+        Mandarinfish,
+        Gobie
+    }
+
+    enum WaterType
+    {
+        Freshwater,
+        Saltwater
+    }
+
+    enum WaterRegion { 
+        Top,
+        Middle,
+        Buttom,
+        All
+
+    }
+    
+}
---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour {
+
+    public float money;
+    public float xp;
+    public int level;
+    public string playerName;
+}
